71ffa06f37b4d5421b4a36d61fdaaa30706e9116
iqboboll <qbal0876@gmail.com>
2026-02-25 09:08:34 +0800

    Added .gitignore for Visual Studio

--- obj/Debug/net10.0/Lab3-OOP.AssemblyInfo.cs
+++ obj/Debug/net10.0/Lab3-OOP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab3-OOP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+983b50faea3a4eecfdae1151378fd3c4c3c6ef06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc3ac3a8d3014a362972df78aa8b16dcccecb462")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab3-OOP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab3-OOP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
